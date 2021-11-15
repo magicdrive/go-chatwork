f3029eece4a59deab69173fc0bb5aa045a16b94c
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-11-15 01:36:49 +0000

    git ignore

--- api/mod.go
+++ api/mod.go
@@ -1,1 +1,32 @@
 package api
+
+import "reflect"
+
+const ApiHost = "https://api.chatwork.com"
+const ApiVersion = "v2"
+
+type ApiSpec struct {
+	Credential  string
+	Method      string
+	ResouceName string
+	Params      map[string]string
+}
+
+func call(spec ApiSpec) (string, error) {
+	return "", nil
+
+}
+
+func StructToMap(data interface{}) map[string]string {
+	result := make(map[string]string)
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		key := elem.Type().Field(i).Name
+		value := elem.Field(i).String()
+		result[key] = value
+	}
+
+	return result
+}
