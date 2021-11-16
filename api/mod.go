7947a846e55418bd8c6507e9a5f475e8476951d2
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-11-16 11:45:09 +0900

    [WIP] client

--- api/mod.go
+++ api/mod.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"reflect"
 )
 
@@ -21,27 +23,60 @@ type ToApiSpecIntf interface {
 	ToApiSpec() ApiSpec
 }
 
-func Call(spec_data ToApiSpecIntf) (string, error) {
-	data := spec_data.ToApiSpec()
+func Call(spec ToApiSpecIntf) ([]byte, error) {
+	data := spec.ToApiSpec()
 
-	req, _ := http.NewRequest(
-		data.Method, fmt.Sprintf("%s/%s/%s", ApiHost, ApiVersion, data.ResouceName), nil)
+	req := HttpRequest(data)
 	req.Header.Set("X-ChatworkToken", data.Credential)
 
-	//TODO:  param & methods
 	client := new(http.Client)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
+
+	if resp, err := client.Do(req); err != nil {
+		return []byte{}, err
+	} else {
+
+		defer resp.Body.Close()
+
+		if body, err := ioutil.ReadAll(resp.Body); err != nil {
+			return []byte{}, err
+		} else {
+			return body, nil
+		}
 	}
-	defer resp.Body.Close()
+}
 
-	byteArray, _ := ioutil.ReadAll(resp.Body)
+func HttpRequest(data ApiSpec) *http.Request {
 
-	return string(byteArray), nil
+	endpoint := fmt.Sprintf("%s/%s/%s", ApiHost, ApiVersion, data.ResouceName)
+
+	if data.Method == http.MethodGet {
+
+		req, _ := http.NewRequest(data.Method, endpoint, nil)
+
+		params := req.URL.Query()
+		for key := range data.Params {
+			params.Add(key, data.Params[key])
+		}
+
+		req.URL.RawQuery = params.Encode()
+		return req
+
+	} else {
+
+		params := url.Values{}
+		for key := range data.Params {
+			params.Add(key, data.Params[key])
+		}
+
+		req, _ := http.NewRequest(data.Method, endpoint, bytes.NewBufferString(params.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		return req
+	}
 }
 
 func StructToMap(data interface{}) map[string]string {
+
 	result := make(map[string]string)
 	elem := reflect.ValueOf(data).Elem()
 	size := elem.NumField()
