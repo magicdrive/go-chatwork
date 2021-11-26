dc316a89ad2607f60f9f0e88008717d20b475249
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-11-26 17:52:08 +0900

    optional int & string

--- optional/int.go
+++ optional/int.go
@@ -2,31 +2,44 @@ package optional
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/goccy/go-json"
 )
 
-type NullableInt struct {
-	Value  int64
-	AsNull bool
+type nullableInt struct {
+	value  int64
+	asNull bool
 }
 
-func Int(v int64, as_null bool) NullableInt {
-	return NullableInt{
-		Value:  v,
-		AsNull: as_null,
+func Int(v int64, as_null bool) nullableInt {
+	return nullableInt{
+		value:  v,
+		asNull: as_null,
 	}
 }
 
-func (c NullableInt) UnmarshalJSON(data []byte) error {
+func (c *nullableInt) Value() (int64, error) {
+	if c.asNull {
+		return 0, errors.New("this value is null.")
+	}
+	return c.value, nil
+}
+
+func (c *nullableInt) IsNull() bool {
+	return c.asNull
+
+}
+
+func (c *nullableInt) UnmarshalJSON(data []byte) error {
 	if bytes.Equal(data, []byte("null")) {
-		c.AsNull = true
+		c.asNull = true
 		return nil
 	}
 
-	if err := json.Unmarshal(data, &c.Value); err != nil {
+	if err := json.Unmarshal(data, &c.value); err != nil {
 
 		var str string
 		if err := json.Unmarshal(data, &str); err != nil {
@@ -37,20 +50,20 @@ func (c NullableInt) UnmarshalJSON(data []byte) error {
 		if err != nil {
 			return fmt.Errorf("A value that cannot be interpreted as a int: %v", err)
 		}
-		c.Value = n
-		c.AsNull = false
+		c.value = n
+		c.asNull = false
 		return nil
 	}
 
-	c.AsNull = true
+	c.asNull = true
 	return nil
 
 }
 
-func (c NullableInt) MarshalJSON() ([]byte, error) {
-	if c.AsNull {
+func (c *nullableInt) MarshalJSON() ([]byte, error) {
+	if c.asNull {
 		return []byte("nil"), nil
 	} else {
-		return []byte(strconv.FormatInt(c.Value, 10)), nil
+		return []byte(strconv.FormatInt(c.value, 10)), nil
 	}
 }
--- optional/string.go
+++ optional/string.go
@@ -1,13 +1,56 @@
 package optional
 
-type NullableString struct {
-	Value  string
-	AsNull bool
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/goccy/go-json"
+)
+
+type nullableString struct {
+	value  string
+	asNull bool
 }
 
-func String(v string, as_null bool) NullableString {
-	return NullableString{
-		Value:  v,
-		AsNull: as_null,
+func String(v string, as_null bool) *nullableString {
+	return &nullableString{
+		value:  v,
+		asNull: as_null,
+	}
+}
+
+func (c *nullableString) Value() (string, error) {
+	if c.asNull {
+		return "", errors.New("this value is null.")
+	}
+	return c.value, nil
+}
+
+func (c *nullableString) IsNull() bool {
+	return c.asNull
+
+}
+
+func (c *nullableString) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, []byte("null")) {
+		c.asNull = true
+		return nil
+	}
+
+	if err := json.Unmarshal(data, &c.value); err != nil {
+		return err
+	}
+
+	c.asNull = true
+	return nil
+
+}
+
+func (c *nullableString) MarshalJSON() ([]byte, error) {
+	if c.asNull {
+		return []byte("null"), nil
+	} else {
+		return []byte(fmt.Sprintf(`"%v"`, c.value)), nil
 	}
 }
