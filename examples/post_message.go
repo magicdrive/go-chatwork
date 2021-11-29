acbe0f4f8e5a26007238b24543699ed81b057d36
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-11-29 19:59:37 +0900

    Fix example CreateTask

--- examples/post_message.go
+++ examples/post_message.go
@@ -1,19 +1,19 @@
-package main
+package example
 
 import (
 	"fmt"
 
 	"github.com/magicdrive/go-chatwork"
-	"github.com/magicdrive/go-chatwork/api"
 	rooms "github.com/magicdrive/go-chatwork/api/resource/rooms_sub"
+	"github.com/magicdrive/go-chatwork/optional"
 )
 
-func main() {
+func PostMessage() {
 
 	client := chatwork.NewChatworkClient(`your-api-key-here`)
 
 	room_id := 1
-	params := rooms.MessagePostParam{Body: "Hi there.", SelfUnread: api.ChatworkBoolTrue}
+	params := rooms.MessagePostParam{Body: "Hi there.", SelfUnread: optional.BoolTrue()}
 
 	if r, err := client.Rooms().Message().Post(room_id, params); err == nil {
 		fmt.Println(r.MessageId)
---
+++ examples/task_create.go
@@ -0,0 +1,36 @@
+package example
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/magicdrive/go-chatwork"
+	"github.com/magicdrive/go-chatwork/api/resource/rooms_sub"
+	"github.com/magicdrive/go-chatwork/optional"
+)
+
+func CreateTask() {
+
+	client := chatwork.NewChatworkClient(`your-api-key-here`)
+
+	task_assign_account_id_1 := 1
+	task_assign_account_id_2 := 2
+
+	date := "2022/01/01"
+	t, _ := time.Parse("2006/01/02", date)
+
+	room_id := 1
+	params := rooms_sub.TaskPostParam{
+		Body:      "Do it now!",
+		Limit:     optional.Int(t.Unix()),
+		LimitType: rooms_sub.TaskLimitTypeDate,
+		ToIds:     []int{task_assign_account_id_1, task_assign_account_id_2},
+	}
+
+	if r, err := client.Rooms().Tasks().Create(room_id, params); err == nil {
+		fmt.Println(r.TaskId)
+	} else {
+		panic(err)
+	}
+
+}
