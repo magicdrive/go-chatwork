78a045bd77e24fa7ec084e2c5aea211d91faf4af
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-12-02 07:11:18 +0000

    Fix incoming_requests resource test

---
+++ test/incoming_requests_test.go
@@ -0,0 +1,130 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/magicdrive/go-chatwork"
+	"github.com/magicdrive/go-chatwork/api"
+	"github.com/magicdrive/go-chatwork/api/resource"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIncomingRequests(t *testing.T) {
+
+	client := chatwork.NewChatworkClient(`test-api-key`)
+
+	target := client.IncomingRequests()
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock_json := `
+	[
+	  {
+		"account_id": 123,
+		"room_id": 322,
+		"name": "John Smith",
+		"chatwork_id": "tarochatworkid",
+		"organization_id": 101,
+		"organization_name": "Hello Company",
+		"department": "Marketing",
+		"avatar_image_url": "https://example.com/abc.png"
+	  }
+	]
+	`
+
+	httpmock.RegisterResponder(http.MethodGet, fmt.Sprintf("%s%s", api.ApiEndpoint, target.ResourceName),
+		httpmock.NewStringResponder(http.StatusOK, mock_json),
+	)
+
+	actual, err := target.List()
+	assert.Nil(t, err)
+
+	expected := make([]resource.IncomingRequestData, 0, 32)
+	err = json.Unmarshal([]byte(mock_json), &expected)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestPutIncomingRequests(t *testing.T) {
+
+	client := chatwork.NewChatworkClient(`test-api-key`)
+
+	request_id := 1
+
+	target := client.IncomingRequests()
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock_json := `
+    {
+      "account_id": 123,
+      "room_id": 322,
+      "name": "John Smith",
+      "chatwork_id": "tarochatworkid",
+      "organization_id": 101,
+      "organization_name": "Hello Company",
+      "department": "Marketing",
+      "avatar_image_url": "https://example.com/abc.png"
+    }
+	`
+
+	httpmock.RegisterResponder(http.MethodPut, fmt.Sprintf("%s%s/%d",
+		api.ApiEndpoint, target.ResourceName, request_id,
+	),
+		httpmock.NewStringResponder(http.StatusOK, mock_json),
+	)
+
+	actual, err := target.Accept(request_id)
+	assert.Nil(t, err)
+
+	expected := resource.IncomingRequestData{}
+	err = json.Unmarshal([]byte(mock_json), &expected)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestDeleteIncomingRequests(t *testing.T) {
+
+	client := chatwork.NewChatworkClient(`test-api-key`)
+
+	request_id := 1
+
+	target := client.IncomingRequests()
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock_json := `
+    {
+      "account_id": 123,
+      "room_id": 322,
+      "name": "John Smith",
+      "chatwork_id": "tarochatworkid",
+      "organization_id": 101,
+      "organization_name": "Hello Company",
+      "department": "Marketing",
+      "avatar_image_url": "https://example.com/abc.png"
+    }
+	`
+
+	httpmock.RegisterResponder(http.MethodDelete, fmt.Sprintf("%s%s/%d",
+		api.ApiEndpoint, target.ResourceName, request_id,
+	),
+		httpmock.NewStringResponder(http.StatusOK, mock_json),
+	)
+
+	actural, err := target.Delete(request_id)
+	assert.Nil(t, err)
+
+	expected := true
+
+	assert.Equal(t, expected, actural)
+}
