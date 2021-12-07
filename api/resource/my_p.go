34faac4c6d4dcb59c9be15beee2d8ecc64659597
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-12-07 20:38:36 +0900

    Fix godoc.

--- api/resource/my_p.go
+++ api/resource/my_p.go
@@ -5,11 +5,13 @@ import (
 	my "github.com/magicdrive/go-chatwork/api/resource/my"
 )
 
+// MyResource chatwork api my resource.
 type MyResource struct {
 	ResourceName string
 	Client       *api.ChatworkApiClient
 }
 
+// NewMyResource new chatwork api my resource.
 func NewMyResource(client *api.ChatworkApiClient) MyResource {
 	data := MyResource{
 		ResourceName: `/my`,
@@ -18,10 +20,12 @@ func NewMyResource(client *api.ChatworkApiClient) MyResource {
 	return data
 }
 
+// Status new chatwork api my/status resource.
 func (c MyResource) Status() my.MyStatusResource {
 	return my.NewMyStatus(c.ResourceName, c.Client)
 }
 
+// Tasks new chatwork api my/tasks resource.
 func (c MyResource) Tasks() my.MyTasksResource {
 	return my.NewMyTasks(c.ResourceName, c.Client)
 }
